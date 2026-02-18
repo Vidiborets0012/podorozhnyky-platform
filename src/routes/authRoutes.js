271e9a4099d26c4edd6cc035e31b70b86575f116
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-18 16:45:45 +0200

    implement session refresh logic with token rotation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,7 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  refreshUserSession,
 } from '../controllers/authController.js';
 import {
   registerUserSchema,
@@ -17,5 +18,7 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
 router.post('/auth/logout', logoutUser);
+
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
