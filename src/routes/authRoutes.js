f80ed1c6d12609b215fa198586bfde8fbfeed688
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-18 16:31:50 +0200

    implement full auth cycle with manual session management and cookies

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
@@ -11,5 +15,7 @@ const router = Router();
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+
+router.post('/auth/logout', logoutUser);
 
 export default router;
