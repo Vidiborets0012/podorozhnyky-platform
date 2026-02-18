a77d559bda6528c41302b3a1c1e9c06c20a25cc7
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-18 09:33:48 +0200

    connect MongoDB using mongoose and add env configuration

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -21,6 +22,8 @@ app.get('/', (req, res) => {
 app.use(notFoundHandler);
 
 app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
