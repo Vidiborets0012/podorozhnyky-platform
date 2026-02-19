32a2c077fa2cd7e3325e28294f1e3f6ff842005e
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-19 16:13:10 +0200

    objectId validator and stories query/params validation

---
+++ src/validations/storyValidation.js
@@ -0,0 +1,16 @@
+import { Joi, Segments } from 'celebrate';
+import { objectIdValidator } from '../utils/objectIdValidator';
+
+export const getStoriesQuerySchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().min(1).default(1),
+    limit: Joi.number().min(1).max(50).default(9),
+    category: Joi.string().custom(objectIdValidator).optional(),
+  }),
+};
+
+export const storyIdParamSchema = {
+  [Segments.PARAMS]: Joi.object({
+    storyId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
