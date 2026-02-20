bba413b45e7a6694ee83169c3be02f883dab80e8
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-20 14:16:14 +0200

    remove unnecessary items from usersRoutes.js

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/authenticate.js';
-import { celebrate } from 'celebrate';
-import {
-  addSavedStoryController,
-  getMyStoriesController,
-  getSavedStoriesController,
-  removeSavedStoryController,
-} from '../controllers/usersController.js';
-import {
-  paginationQuerySchema,
-  storyIdParamSchema,
-} from '../validations/storyValidation.js';
+// import { celebrate } from 'celebrate';
+// import {
+//   addSavedStoryController,
+//   getMyStoriesController,
+//   getSavedStoriesController,
+//   removeSavedStoryController,
+// } from '../controllers/usersController.js';
+// import {
+//   paginationQuerySchema,
+//   storyIdParamSchema,
+// } from '../validations/storyValidation.js';
 
 const router = Router();
 
@@ -19,36 +19,36 @@ router.get('/me', authenticate, (req, res) => {
 });
 
 // POST /api/users/me/saved-stories/:storyId
-router.post(
-  '/me/saved-stories/:storyId',
-  authenticate,
-  celebrate(storyIdParamSchema),
-  addSavedStoryController,
-);
+// router.post(
+//   '/me/saved-stories/:storyId',
+//   authenticate,
+//   celebrate(storyIdParamSchema),
+//   addSavedStoryController,
+// );
 
 //DELETE /api/users/me/saved-stories/:storyId
-router.delete(
-  '/me/saved-stories/:storyId',
-  authenticate,
-  celebrate(storyIdParamSchema),
-  removeSavedStoryController,
-);
+// router.delete(
+//   '/me/saved-stories/:storyId',
+//   authenticate,
+//   celebrate(storyIdParamSchema),
+//   removeSavedStoryController,
+// );
 
 //GET /api/users/me/saved-stories + пагінація
-router.get(
-  '/me/saved-stories',
-  authenticate,
-  celebrate(paginationQuerySchema),
-  getSavedStoriesController,
-);
+// router.get(
+//   '/me/saved-stories',
+//   authenticate,
+//   celebrate(paginationQuerySchema),
+//   getSavedStoriesController,
+// );
 
 // Приватний ендпоінт для отримання власних історій автора + пагінація
 // GET /api/users/me/stories;
-router.get(
-  '/me/stories',
-  authenticate,
-  celebrate(paginationQuerySchema),
-  getMyStoriesController,
-);
+// router.get(
+//   '/me/stories',
+//   authenticate,
+//   celebrate(paginationQuerySchema),
+//   getMyStoriesController,
+// );
 
 export default router;
